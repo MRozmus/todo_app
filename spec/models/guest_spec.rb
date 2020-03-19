9fe27db44927d20a7d755624f6ffd41988e8a12e
MRozmus <marcin.rozmus97@gmail.com>
2020-03-19 12:49:52 +0100

    Add guest create test

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Guest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "on create" do
+    it "should save" do
+      expect { Guest.create }.to change { Guest.count }
+    end
+  end
 end
