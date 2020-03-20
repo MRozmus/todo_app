308a81381c1ba6eedee474be45ecdc40290e70fd
MRozmus <marcin.rozmus97@gmail.com>
2020-03-20 10:50:55 +0100

    Add todo association & test for guest

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Guest, type: :model do
       expect { Guest.create }.to change { Guest.count }
     end
   end
+  context "todo association" do
+    it "should be able to find models record" do
+      guest = Guest.create
+      todo = Todo.create(title: "example_title", description: "example_description", priority: rand(0..2), status: [true, false].sample, guest_id: guest.id)
+      expect(guest.todos).to include(todo)
+    end
+  end
 end
