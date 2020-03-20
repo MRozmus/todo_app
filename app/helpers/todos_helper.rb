1ce5b1f5521d84426d94634b3b2a316454feceb7
MRozmus <marcin.rozmus97@gmail.com>
2020-03-20 12:43:28 +0100

    Add helpers & update views

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,2 +1,18 @@
 module TodosHelper
+  
+  def read_priority(priority)
+    case priority
+    when 0
+      "low"
+    when 1
+      "medium"
+    when 2
+      "high"
+    end
+  end
+
+  def read_status(status)
+    status == true ? "done" : "to do"
+  end
+  
 end
