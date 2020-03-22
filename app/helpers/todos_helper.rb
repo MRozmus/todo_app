31782cea1b1d3ba79866e59119422d2c375b353b
MRozmus <marcin.rozmus97@gmail.com>
2020-03-22 14:09:16 +0100

    Add styles & update views

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -3,16 +3,51 @@ module TodosHelper
   def read_priority(priority)
     case priority
     when 0
-      "low"
+      "Low"
     when 1
-      "medium"
+      "Medium"
     when 2
-      "high"
+      "High"
     end
   end
 
   def read_status(status)
-    status == true ? "done" : "to do"
+    status == true ? "Done" : "To Do"
   end
   
+  def bg_status(status)
+    "bg-card-done" if status == true
+  end
+
+  def time_ago(time)
+    seconds = Time.now.to_i - time.to_i
+    if seconds / 60 >= 1
+      minutes = seconds / 60
+      if minutes / 60 >= 1
+        hours = minutes / 60
+        if hours / 60 >= 1
+          days = hours / 24
+          return (days == 1 ? "1 day" : "#{days} days") + "#{hours} h ago"
+        else
+          return "#{hours} h #{minutes % 60} min ago"
+        end
+      else
+        return "#{minutes} min #{seconds % 60} sec ago"
+      end
+    else
+    return "#{seconds} sec ago"
+    end
+  end
+
+  def bg_priority(priority)
+    case priority
+    when 0
+      "bg-prio-low"
+    when 1
+      "bg-prio-medium"
+    when 2
+      "bg-prio-high"
+    end
+  end
+
 end
