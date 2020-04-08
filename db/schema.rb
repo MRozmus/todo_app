35ae591b3f49d8bde8c5134ec7e8f93f62a1678b
Marcin Rozmus <marcin.rozmus97@gmail.com>
2020-04-08 20:05:43 +0200

    Added user_id to todo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_05_120610) do
+ActiveRecord::Schema.define(version: 2020_04_08_175636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_04_05_120610) do
     t.bigint "guest_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["guest_id"], name: "index_todos_on_guest_id"
   end
 
