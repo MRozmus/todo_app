635c85d6d30157eb758aa48e19ba9dff0d11ac08
Marcin Rozmus <marcin.rozmus97@gmail.com>
2020-04-11 19:00:25 +0200

    Added guest deletion after 24h

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -53,6 +53,7 @@ class TodosController < ApplicationController
   private
 
   def set_guest
+    guest_expire
     if session[:guest_id].nil?
       guest = Guest.create
       session[:guest_id] = guest.id
@@ -86,6 +87,19 @@ class TodosController < ApplicationController
       @sort = params[:sort]
     else
       @sort = "title"
+    end
+  end
+
+  def guest_expire
+    unless session[:guest_id].nil?
+      guest = Guest.find(session[:guest_id])
+      if (Time.now.to_i - guest.created_at.to_i) > 86399
+        guest.todos.each do |todo|
+          todo.delete if todo.user_id == nil
+        end
+        guest.delete
+        session[:guest_id] = nil
+      end
     end
   end
 end
