1f13e1291d2b9e8771c2d0ad3c4c47a6c258a9fc
Marcin Rozmus <marcin.rozmus97@gmail.com>
2020-04-17 00:27:44 +0200

    Added more relation validations

--- app/validators/relation_validator.rb
+++ app/validators/relation_validator.rb
@@ -1,7 +1,17 @@
 class RelationValidator < ActiveModel::Validator
   def validate(record)
     if record.sender_id == record.receiver_id
-      record.errors[:base] << "Sender id couldn't be equal to receiver id"
+      record.errors[:base] << "Sender email is equal to receiver's"
+    end
+    if Relation.find_by(sender_id: record.sender_id, receiver_id: record.receiver_id, status: false).present?
+      record.errors[:base] << "Request was sent already"
+    end
+    if Relation.find_by(sender_id: record.receiver_id, receiver_id: record.sender_id, status: false).present?
+      record.errors[:base] << "Request was sent to you already"
+    end
+    if Relation.find_by(sender_id: record.sender_id, receiver_id: record.receiver_id, status: true).present? ||
+      Relation.find_by(sender_id: record.receiver_id, receiver_id: record.sender_id, status: true).present?
+    record.errors[:base] << "You are a friends already"
     end
   end
 end
