079051f3022bbdeb64edb07fabfe29de0359b5f5
Marcin Rozmus <marcin.rozmus97@gmail.com>
2020-04-19 13:25:52 +0200

    Added quantity of relation request

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def requests?(current_user_id)
+    relations = Relation.where(receiver_id: current_user_id, status: false).count
+    relations == 0 ? "" : " <span class='badge badge-info'>#{relations}</span>"
+  end
+
 end
