2fbf3f09d847d24cda23de6c84758e5c3b742bad
Marcin Rozmus <marcin.rozmus97@gmail.com>
2020-04-19 13:35:25 +0200

    Added quantity of received todos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     relations == 0 ? "" : " <span class='badge badge-info'>#{relations}</span>"
   end
 
+  def shares_count(current_user_id)
+    todo_ids = []
+    Share.where(receiver_id: current_user.id).each do |share|
+      todo_ids << share.todo_id
+    end
+    " <span class='badge badge-light'>#{Todo.where(id: todo_ids).count}</span>"
+  end
+
 end
