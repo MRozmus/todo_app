d852f8c1744bc8a9ec3e97dec6e0991627a4f547
Marcin Rozmus <marcin.rozmus97@gmail.com>
2020-04-22 20:46:42 +0200

    Created users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :todos
+  has_many :sent_relations, class_name: 'Relation', foreign_key: 'sender_id'
+  has_many :received_relations, class_name: 'Relation', foreign_key: 'recipient_id'
+  has_many :sent_shares, class_name: 'Share', foreign_key: 'sender_id'
+  has_many :received_shares, class_name: 'Share', foreign_key: 'recipient_id'
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
 end
