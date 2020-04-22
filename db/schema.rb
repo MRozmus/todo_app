8d840dd18bd7bae62f3eaae20e67c2badb9f987e
Marcin Rozmus <marcin.rozmus97@gmail.com>
2020-04-22 20:15:24 +0200

    Created guests table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_132756) do
+ActiveRecord::Schema.define(version: 2020_03_19_114056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,51 +18,6 @@ ActiveRecord::Schema.define(version: 2020_04_18_132756) do
   create_table "guests", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "relations", force: :cascade do |t|
-    t.integer "sender_id"
-    t.integer "receiver_id"
-    t.boolean "status", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "shares", force: :cascade do |t|
-    t.integer "sharer_id"
-    t.integer "relation_id"
-    t.integer "todo_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "receiver_id"
-  end
-
-  create_table "todos", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.integer "priority"
-    t.boolean "status"
-    t.bigint "guest_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.index ["guest_id"], name: "index_todos_on_guest_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
